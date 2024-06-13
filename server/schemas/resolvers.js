d5800179292ab60b1e135b9d045a8bf23e4c84c1
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-06-12 21:36:33 -0500

    wrote mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -7,17 +8,36 @@ const resolvers = {
         },
     },
     Mutation: {
-        login: async (parent, {  }) => {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ message: "Can't find this user" });
+            }
 
+            const correctPw = await user.isCorrectPassword(body.password);
+
+            if (!correctPw) {
+                return res.status(400).json({ message: 'Wrong password!' });
+            }
+            const token = signToken(user);
+            res.json({ token, user });
         },
-        addUser: async (parent, {  }) => {
-
+        addUser: async (parent, { username, email, password }) => {
+            return await User.create({ username, email, password });
         },
-        saveBook: async (parent, {  }) => {
-
+        saveBook: async (parent, { user, body }) => {
+            return await User.findOneAndUpdate(
+                { _id: user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+            )
         },
-        removeBook: async (parent, {  }) => {
-
+        removeBook: async (parent, { user, params }) => {
+            return await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+            )
         },
     },
 };
