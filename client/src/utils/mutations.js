26a5a69dcb7053588caf2c7c7ff874ff88cd2c53
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-06-12 21:57:18 -0500

    added mutations and queries to frontend

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,18 @@
+//   * `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+
+//   * `ADD_USER` will execute the `addUser` mutation.
+
+//   * `SAVE_BOOK` will execute the `saveBook` mutation.
+
+//   * `REMOVE_BOOK` will execute the `removeBook` mutation.
+
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql``;
+
+export const ADD_USER = gql``;
+
+export const SAVE_BOOK = gql``;
+
+export const REMOVE_BOOK = gql``;
+
